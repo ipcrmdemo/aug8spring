9c53ff53e7c94aa2f6f4cfe7ed336a1d3078b6db
Matt Cadorette <mcadorette@gmail.com>
2019-08-08 10:33:10 -0400

    Initial commit from Atomist
    [atomist:generated] [atomist:generator=create-spring]

---
+++ src/main/java/com/aug8spring/Aug8springApplication.java
@@ -0,0 +1,12 @@
+package com.aug8spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aug8springApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aug8springApplication.class, args);
+	}
+}
